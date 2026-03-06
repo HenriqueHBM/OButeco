481875d340ddb611a78ab329408318b86ea20d10
João <joaovictormelo43@gmail.com>
2026-03-05 21:05:47 -0300

    Minhas Alterações

--- OButeco_/src/main/java/buteco/controller/estoque/EstoqueController.java
+++ OButeco_/src/main/java/buteco/controller/estoque/EstoqueController.java
@@ -1,19 +1,36 @@
 package buteco.controller.estoque;
 
-import java.util.Scanner;
+import buteco.model.estoque.Estoque;
+import buteco.view.EstoqueView;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class EstoqueController {
-    Scanner sc = new Scanner(System.in);
+    private EstoqueView view;
+    static List<Estoque> estoques;
+
+    public EstoqueController() {
+
+    }
+
 
     public void index(){
 
         int opcao = 0;
 
         do{
-            System.out.println("[1] CADASTRAR ENTRADA; [2] - LISTAR ESTOQUE; [3] - MOVIMENTACOES ENTRADAS; [4] - MOVIMENTACOES SAIDAS;  [0] - SAIR");
-            opcao = sc.nextInt();
+            opcao = view.exibirMenu();
+            switch (opcao){
+                case 1 -> cadastrarEntrada();
+            }
 
         }while(opcao != 0 );
 //        sc.close();
+
+    }
+
+    public void cadastrarEntrada(){
+
     }
 }
