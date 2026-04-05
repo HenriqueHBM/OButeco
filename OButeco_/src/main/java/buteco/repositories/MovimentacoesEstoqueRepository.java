c08e067b62ea606d9c39f6d6b7668ce31ee9310b
Jonas-Lacerda <jonaslacerdap@gmail.com>
2026-04-05 00:47:30 -0300

    tabelas e repository  estoque e movimentacoesestoque alteradas

---
+++ OButeco_/src/main/java/buteco/repositories/MovimentacoesEstoqueRepository.java
@@ -0,0 +1,33 @@
+package buteco.repositories;
+
+import buteco.model.movimentacoes.MovimentacoesEstoque;
+import jakarta.persistence.EntityManager;
+
+public class MovimentacoesEstoqueRepository {
+    private EntityManager em;
+
+    public MovimentacoesEstoqueRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public MovimentacoesEstoque findById(Long id){
+        return em.find(MovimentacoesEstoque.class, id);
+    }
+
+    public void create(MovimentacoesEstoque movimentacoesEstoque){
+        em.getTransaction().begin();
+        em.persist(movimentacoesEstoque);
+        em.getTransaction().commit();
+    }
+
+    public void update(MovimentacoesEstoque movimentacoesEstoque){
+        em.getTransaction().begin();
+        em.persist(movimentacoesEstoque);
+        em.getTransaction().commit();
+    }
+
+    public void delete(MovimentacoesEstoque movimentacoesEstoque){
+        em.remove(em.contains(movimentacoesEstoque) ? movimentacoesEstoque : em.merge(movimentacoesEstoque);
+    }
+
+}
